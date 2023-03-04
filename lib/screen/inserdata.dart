1433a8385ae88b73e79eeb8b5182abab9fb694fc
Redzuan Shah <cumatekan@gmail.com>
2023-03-04 12:53:22 +0800

    update insert data

--- lib/screen/inserdata.dart
+++ lib/screen/inserdata.dart
@@ -8,11 +8,57 @@ class InsertData extends StatefulWidget {
 }
 
 class _InsertDataState extends State<InsertData> {
+  final fromNameController = TextEditingController();
+  final toNameController = TextEditingController();
+  final memoController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Inser Data"),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(9),
+          child: Column(
+            children: [
+              SizedBox(height: 50,),
+              TextField(
+                controller: fromNameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'From',
+                  hintText: 'From :'
+                ),
+              ),
+              SizedBox(height: 30,),
+              TextField(
+                controller: toNameController,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'To',
+                    hintText: 'To :'
+                ),
+              ),
+              SizedBox(height: 30,),
+              TextField(
+                controller: memoController,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Memo',
+                    hintText: 'Memo :'
+                ),
+              ),
+              SizedBox(height: 30,),
+              MaterialButton(onPressed: (){
+              },
+                child: const Text('Submit Memo'),
+                color: Colors.blue,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
